f998e455ed53b85bc60e6c995e01d254d0bb9bd8
Clark Alesna <clark_alesna@hotmail.com>
2023-02-09 18:55:19 +0800

    added teddyswap test contracts variants hashes

--- src/amm/scripts.ts
+++ src/amm/scripts.ts
@@ -7,9 +7,9 @@ export type OrderAddrs = {
 }
 
 export const OrderAddrsV1Testnet: OrderAddrs = {
-  ammDeposit: "addr_test1wqr4uz0tp75fu8wrg6gm83t20aphuc9vt6n8kvu09ctkugq6ch8kj",
-  ammRedeem: "addr_test1wzpa5704x8qel88ympf4natfdzn59nc9esj7609y3sczmmsxcd9lz",
-  ammSwap: "addr_test1wz4900hxeyet6nj6d5acp9vygpeczed9jn69pv4z96py45czkehtj"
+  ammDeposit: "addr_test1wr8h2meedj6ww948lq92x39gp99d2jh2ve40zgnp36782tqm2usdc",
+  ammRedeem: "addr_test1wz3wqe54kf4fvuw77jtg2z7jftqum74glkfsxmv5rwvredc4qvcvr",
+  ammSwap: "addr_test1wr623ag5vs3sz0gxlwnskemuqujssxvpstk7ayt48729ltsa47uze"
 }
 
 export type ScriptCreds = {
@@ -20,9 +20,9 @@ export type ScriptCreds = {
 }
 
 export const ScriptCredsV1: ScriptCreds = {
-  ammPool: "a258f896dff1d01ac9a8bd0598304b933a8f3e9e0953938767178099",
-  ammDeposit: "075e09eb0fa89e1dc34691b3c56a7f437e60ac5ea67b338f2e176e20",
-  ammRedeem: "83da79f531c19f9ce4d85359f56968a742cf05cc25ed3ca48c302dee",
-  ammSwap: "aa57bee6c932bd4e5a6d3b80958440738165a594f450b2a22e824ad3"
+  ammPool: "36170eeadfc1d7d72ada62c19dc51c9656f1ed0cc93c1da2c1e900a2",
+  ammDeposit: "cf756f396cb4e716a7f80aa344a8094ad54aea666af122618ebc752c",
+  ammRedeem: "a2e06695b26a9671def496850bd24ac1cdfaa8fd93036d941b983cb7",
+  ammSwap: "f4a8f5146423013d06fba70b677c072508198182edee91753f945fae"
 }
 
